8b81e2a993972d24c0798926923b9cd3363ade58
“Remo <remoharsono@yahoo.com>
2022-12-06 13:36:52 +0700

    update

--- app/src/main/java/averin/sirs/com/DetailMR.java
+++ app/src/main/java/averin/sirs/com/DetailMR.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -27,22 +25,13 @@ import averin.sirs.com.Adapter.RequestHandler;
 
 public class DetailMR extends AppCompatActivity {
 
-    String val_token, kodeklinik, idRegist, no_ktp, namaKlinik, namaDokter, jenisPoli, tglPeriksa, keadaan_umum, tekanan_darah, suhu, tinggi_badan,
-            kesadaran, nadi, pernafasan, bb, tindakan, nama_icd10, kd_resep, nama_obat, note_obat, jml_obat, aturan_pakai, ket_obat;
+    String val_token, no_ktp, kd_klinik, idRegist, namaDokter, jenisPoli, tglPeriksa, keadaan_umum, tekanan_darah, suhu, tinggi_badan,
+            kesadaran, nadi, pernafasan, bb, tindakan, nama_icd10, kd_resep;
     TextView txt_namapasien, txt_nomr, txt_namadokter, txt_namapoli, txt_tglperiksa, txt_KeadaanUmum, txt_TekananDarah, txt_suhu,
-            txt_tinggi, txt_kesadaran, txt_nadi, txt_pernafasan, txt_bb, txt_nmobat,
-            txt_jmlobat, txt_aturan, txt_noteobat, txt_ketobat, txt_no,
-            txt_info_null;
+            txt_tinggi, txt_kesadaran, txt_nadi, txt_pernafasan, txt_bb, txt_nmobat1, txt_nmobat2, txt_nmobat3,
+            txt_jmlobat1,txt_jmlobat2,txt_jmlobat3, txt_aturan1,txt_aturan2,txt_aturan3,txt_noteobat1,txt_noteobat2,
+            txt_noteobat3, txt_ketobat1,txt_ketobat2,txt_ketobat3;
     EditText edt_tindakan, edt_icd10;
-
-    //Dialog Confirm
-    AlertDialog.Builder dial_builder;
-    AlertDialog dial_info_null;
-    LayoutInflater inflater;
-    View view_null_data;
-
-    String APIurl = RequestHandler.APIdev;
-    public String urlDetailMR = APIurl+"/api/vi/get-detail-riwayat.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,25 +89,6 @@ public class DetailMR extends AppCompatActivity {
         txt_ketobat1 = findViewById(R.id.ketobat1);
         txt_ketobat2 = findViewById(R.id.ketobat2);
         txt_ketobat3 = findViewById(R.id.ketobat3);
-
-        //Dialog Empty Data
-        ViewGroup vg = findViewById(android.R.id.content);
-        dial_builder = new AlertDialog.Builder(DetailMR.this,R.style.CustomAlertDialog);
-        inflater = getLayoutInflater();
-        txt_info_null = view_null_data.findViewById(R.id.txt_info_failed);
-        view_null_data = inflater.inflate(R.layout.dialog_failed_regist, vg, false);
-        Button btn_ok = view_null_data.findViewById(R.id.btn_oke_failed);
-        dial_builder.setView(view_null_data);
-        dial_info_null = dial_builder.create();
-        dial_info_null.setCancelable(false);
-        btn_ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dial_info_null.dismiss();
-                Intent i = new Intent(DetailMR.this, MRpasienActivity.class);
-                startActivity(i);
-            }
-        });
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -211,111 +181,5 @@ public class DetailMR extends AppCompatActivity {
         edt_tindakan.setText(tindakan);
         edt_icd10.setText(nama_icd10);
 
-    }
-    private void getDetailMR() {
-        final String iniToken = val_token;
-        final String kd_klinik = kodeklinik;
-        final String Regid = idRegist;
-
-        class ambilDataMR extends AsyncTask<Void, Void, String> {
-
-            private ProgressBar progressBar;
-
-            @Override
-            protected String doInBackground(Void... voids) {
-                //creating request handler object
-                RequestHandler requestHandler = new RequestHandler();
-
-                //creating request parameters
-                HashMap<String, String> params = new HashMap<>();
-                params.put("kode_klinik", kodeklinik);
-                params.put("idReg", Regid);
-
-                //returing the response
-                return requestHandler.requestData(urlDetailMR, "POST", "application/json; charset=utf-8", "X-Api-Token",
-                        iniToken, "X-Px-Key", "", params);
-            }
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-//                progressBar = findViewById(R.id.progressBar);
-//                progressBar.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-//                progressBar.setVisibility(View.GONE);
-
-                try {//converting response to json object
-                    JSONObject jr = new JSONObject(s);
-                    //if no error in response
-                    if (jr.getString("code").equals("500")) {
-                        txt_info_null.setText(jr.getString("msg"));
-                        dial_info_null.show();
-                    } else if(jr.getString("code").equals("200")){
-                        JSONArray res_vital_sign = jr.getJSONArray("vital_sign");
-                        JSONArray res_tindakan = jr.getJSONArray("tindakan");
-                        JSONArray res_icd10 = jr.getJSONArray("icd10");
-                        JSONArray res_resep = jr.getJSONArray("resep");
-                        int no = 1;
-
-//                        Vital Sign
-                        for(int vs = 0; vs < res_vital_sign.length(); vs++) {
-                            JSONObject obj_vs = res_vital_sign.getJSONObject(vs);
-                            keadaan_umum = obj_vs.getString("keadaan_umum");
-                            tekanan_darah = obj_vs.getString("tekanan_darah");
-                            suhu = obj_vs.getString("suhu");
-                            tinggi_badan = obj_vs.getString("tinggi_badan");
-                            kesadaran = obj_vs.getString("kesadaran_pasien");
-                            nadi = obj_vs.getString("nadi");
-                            pernafasan = obj_vs.getString("pernafasan");
-                            bb = obj_vs.getString("berat_badan");
-
-                            txt_KeadaanUmum.setText(keadaan_umum);
-                            txt_TekananDarah.setText(tekanan_darah);
-                            txt_suhu.setText(suhu);
-                            txt_tinggi.setText(tinggi_badan);
-                            txt_kesadaran.setText(kesadaran);
-                            txt_nadi.setText(nadi);
-                            txt_bb.setText(bb);
-                        }
-
-//                        Tindakan
-                        for(int td = 0; td < res_tindakan.length(); td++) {
-                            JSONObject obj_tdk = res_tindakan.getJSONObject(td);
-//                            no++;
-                            tindakan = obj_tdk.getString("nama_tindakan");
-                            edt_tindakan.setText(no++ + ". "+tindakan);
-                        }
-
-//                        ICD-10
-                        for(int icd = 0; icd < res_icd10.length(); icd++) {
-                            JSONObject obj_icd = res_icd10.getJSONObject(icd);
-//                            no++;
-                            nama_icd10 = obj_icd.getString("nama_icd");
-                            edt_icd10.setText(no++ + ". "+ nama_icd10);
-                        }
-
-//                        Resep Obat
-                        for(int ro = 0; ro < res_resep.length(); ro++) {
-                            JSONObject obj_ro = res_resep.getJSONObject(ro);
-//                            no++;
-                            nama_obat = obj_ro.getString("nama_brg");
-                            note_obat = obj_ro.getString("note");
-                            jml_obat = obj_ro.getString("jumlah_pesan");
-                            aturan_pakai = obj_ro.getString("nama_dosis");
-                            ket_obat = obj_ro.getString("ket");
-                            txt_nmobat.setText(no++ + ". "+ nama_icd10);
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        ambilDataMR pl = new ambilDataMR();
-        pl.execute();
     }
 }
